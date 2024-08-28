68176f402dd9717d863f3933da35c5fcf5fc2b89
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-08-28 23:08:55 +0300

    minor corrections

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -15,8 +15,8 @@ import org.hibernate.annotations.SQLRestriction;
 @Entity
 @Getter
 @Setter
-@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
-@SQLRestriction("is_deleted = false")
+@SQLDelete(sql = "UPDATE books SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction("is_deleted = FALSE")
 @Table(name = "books")
 public class Book {
     @Id
